3cf3617d82c6949fa28dac0fb599ae6b3bdc5465
Jane <jane.kansiree@gmail.com>
2017-07-26 11:11:30 +0700

    add my change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const model = require('./Model')
 const controller = require('./Controller/walletController')
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("server start")
 })
 
@@ -13,17 +13,17 @@ app.get('/create', (req, res) => {
     res.send('Hello ')
 })
 
-app.get("/insert",(req,res)=>{
-    model.account.create({name : "kinnch"})
+app.get("/insert", (req, res) => {
+    model.account.create({ name: "kinnch" })
     res.send("asd")
 })
 
-app.get("/query/:id",(req,res)=>{
-    controller.getAccountInfo(req.query.id).then((accounts)=>{
-            res.send(accounts[0])  
+app.get("/query/:id", (req, res) => {
+    controller.getAccountInfo(req.query.id).then((accounts) => {
+        res.send(accounts[0])
     })
 })
- 
+
 app.listen(3000, () => {
     console.log("app listen port 3000")
 })
