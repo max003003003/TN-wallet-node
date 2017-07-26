4088237c950631016f52ea4b7978381296293517
vorabhol chauybhan <vorabhol.c@ku.th>
2017-07-26 14:29:53 +0700

    implement create transaction with DB transaction

--- Controller/walletController.js
+++ Controller/walletController.js
@@ -1,17 +1,36 @@
 const model = require('../Model')
+const account = require('./account')
 module.exports = {
-    getAccountInfo: (account_id,attributes) =>{
+    getAccountInfo: (account_id, attributes) => {
         return model.account.findAll({
-            where : {
+            where: {
                 account_id: account_id
             },
-            attributes : attributes
+            attributes: attributes
         })
     },
 
     insertAccount: (account) => {
         return model.account.create(account)
+    },
+  
+
+    insertTransaction: (transactionobj) => {
+        return model.sequelize.transaction((t) => {
+            return model.transaction.create(transactionobj, { transaction: t })
+                .then((result) => {
+                    p1 = account.updateAccountBalance2("1233")                    
+                    p2 = account.updateAccountBalance2("1222")
+                    Promise.all([p1, p2]).then(values => {
+                        return values
+                    }).catch(error => {
+                            throw new error.toString()
+                        })
+                })
+        }).then((result) => {
+            return result
+        }).catch((error) => {
+            console.log(error.toString())
+        })
     }
-
-
 }
--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.get('/create', (req, res) => {
     res.send('Hello ')
 })
 
+
+
 app.get("/insert", (req, res) => {        
      
      const account ={
@@ -33,6 +35,7 @@ app.get("/insert", (req, res) => {
        
 })
 
+
 app.get("/accounts/:id", (req, res) => {
     controller.getAccountInfo(req.params.id,['account_id','name','surname']).then((accounts) => {
         res.send(accounts)
@@ -43,6 +46,35 @@ app.get("/balances/:id", (req, res) => {
         res.send(accounts)
     })
 })
+app.get("/transfer",(req,res)=>{
+    // const trans = {
+    //     type: req.body.type,
+    //     src_acc_id: req.body.src_acc_id,
+    //     src_initial_balance: req.body.src_initial_balance,
+    //     des_acc_id:req.body.des_acc_id,
+    //     des_initial_balance: req.body.des_initial_balance,
+    //     amount: req.body.amount,
+    //     fee: req.body.fee,
+    //     src_remain_balance: req.body.src_remain_balance,
+    //     des_remain_balance: req.body.des_remain_balance
+    // }
+
+    const trans = {
+        
+        type: "transfer",
+        src_acc_id: 6302335476,
+        src_initial_balance: 4700,
+        des_acc_id: 7582983660,
+        des_initial_balance: 4200,
+        amount: 500 ,
+        fee: 0 ,
+        src_remain_balance: 4500,
+        des_remain_balance: 4300
+
+    }
+   controller.insertTransaction(trans)
+})
+
 app.listen(3000, () => {
     console.log("app listen port 3000")
 })
