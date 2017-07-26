6a54fa6fada406f96c43f45f2631bcbe915aa61b
vorabhol chauybhan <vorabhol.c@ku.th>
2017-07-26 11:05:44 +0700

    implement tb_transactions structure

--- Model/index.js
+++ Model/index.js
@@ -1,29 +1,32 @@
 const Sequelize = require('sequelize')
- 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            }})
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+})
 
 var account_model = sequelize.import('./accountModel')
-var transaction_model = require.import('./transactionModel')
-var db ={
- account : account_model,
- transaction: transaction_model,
- sequelize : sequelize,
- Sequelize : Sequelize,
- createTable: ()=>{
-      account_model.drop({force:true})
-      account_model.sync({force: true})
- }
- 
- 
+var transaction_model = sequelize.import('./transactionModel')
+var db = {
+    account: account_model,
+    transaction: transaction_model,
+    sequelize: sequelize,
+    Sequelize: Sequelize,
+    createTable: () => {
+        account_model.drop({ force: true })
+        account_model.sync({ force: true })
+        transaction_model.drop({ force: true })
+        transaction_model.sync({ force: true })
+    }
+
+
 }
 
 
 
-module.exports= db
+module.exports = db
--- Model/transactionModel.js
+++ Model/transactionModel.js
@@ -1,6 +1,46 @@
 module.exports = function (sequelize, DataTypes) {
     const transaction = sequelize.define('tb_transaction', {
-        
+        type: {
+            type: DataTypes.STRING,
+            require: true
+        },
+        src_account_id: {
+            type: DataTypes.INTEGER(10),
+            require: true
+        },
+        src_initial_balance: {
+            type: DataTypes.DOUBLE,
+            require: true
+        },
+        des_account_id: {
+            type: DataTypes.INTEGER(10),
+            require: true
+        },
+        des_initial_balance: {
+            type: DataTypes.DOUBLE,
+            require: true
+        },
+        amount: {
+            type: DataTypes.DOUBLE,
+            require: true
+        },
+        fee: {
+            type: DataTypes.DOUBLE,
+            require: true,
+            defaultValue: 0.00
+        },
+        src_remain_balance: {
+            type: DataTypes.DOUBLE,
+            require: true
+        },
+        des_remain_balance: {
+            type: DataTypes.DOUBLE,
+            require: true
+        },
+        transaction_timestamp: {
+            type: DataTypes.DATE,
+            require: true
+        }
     })
     return transaction
 }
