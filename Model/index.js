8c53d152456da7b0a811cb1c3734106eeff8c1e8
Bonn <aphpbonn@gmail.com>
2017-07-26 11:34:54 +0700

    add balances.robot file for testing /balances and rename account.robot to accounts.robot

--- Model/index.js
+++ Model/index.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000
-            }})
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+})
 
 var account_model = sequelize.import('./accountModel')
 var transaction_model = sequelize.import('./transactionModel')
-
  
 var db = {
     account: account_model,
