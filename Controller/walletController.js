67477c7ae9a99c7df56d0bae4cf70f7240e75dc5
Jittarin R <Jittarin8857@gmail.com>
2017-07-26 11:55:12 +0700

    ccccc

--- Controller/walletController.js
+++ Controller/walletController.js
@@ -1,22 +1,11 @@
 const model = require('../Model')
 module.exports = {
-<<<<<<< HEAD
-    getAccountInfo: (account_id) => {
-        return model.account.findAll({ id: account_id })
-    },
-
-    getNameSurnameID: (account_id) =>{
-        return model.account.findOne({
-            where: {account_id: account_id},
-            attributes: ['name']
-=======
     getAccountInfo: (account_id,attributes) =>{
         return model.account.findAll({
             where : {
                 account_id: account_id
             },
             attributes : attributes
->>>>>>> 28f83f5ba3ae43977b15ab718df39f4b3bd4c11c
         })
     },
 
