a0d2b2e071b5efa42498c56bb462bc91c9cedb8b
Jane <jane.kansiree@gmail.com>
2017-07-26 14:10:56 +0700

    fixed checkLimitBalance

--- Controller/walletController.js
+++ Controller/walletController.js
@@ -13,17 +13,15 @@ module.exports = {
         return model.account.create(account)
     },
 
-    checkLimitBalance: (account_id) =>{
-        var acc = model.account.findOne({
+    checkLimitBalance: async (account_id) =>{
+        const limit = 5000
+        var acc = await model.account.findOne({
             where:{
                 destinationAccountID: account_id
             }
             
         })
         var sum = acc.destinationInitialBalance + acc.amount
-        var result = 5000 - sum
-        if (result < 0)
-            return false
-        return true
+        return (limit - sum) < 0 ? false : true
     }
 }
