268a413d8e50a6307bca9efc262f66bf8012cf4d
Jane <jane.kansiree@gmail.com>
2017-07-27 10:49:43 +0700

    my change

--- Controller/walletController.js
+++ Controller/walletController.js
@@ -30,44 +30,22 @@ function checkAccountExist(account_id) {
             account_id: account_id
         }
         }).then((account)=>{
-            if( (account.length === 0) ){
-                return false
-            }else{
+            if( !(account.length === 0) ){
                 return true
+            }else{
+                return false
             }
         })
     
 }
-function checkEnoughBalance(account_id,amount){
-
-    return model.account.findAll({
-        where:{
-            account_id: account_id
-        },
-        attributes: ['balance']
-    }).then((balance)=>{
-        if (balance[0].dataValues.balance >= amount) return true
-        else return false
-    })
-}
-
-function checkLimitBalance(account_id,amount) {
+function checkLimitBalance(account_id) {
     const limit = 5000
-    // var acc = model.transaction.findAll({
-    //     where: {
-    //         destinationAccountID: account_id
-    //     }
-
-    // })
-    // var sum = acc.destinationInitialBalance + acc.amount
-    // return (limit - sum) < 0 ? false : true
-
-    return model.account.findAll({
+    return model.transaction.findAll({
         where: {
-            account_id: account_id
+            destinationAccountID: account_id
         }
-        }).then((account)=>{
-           let sum = account.balance + amount
+        }).then((transaction)=>{
+           let sum = destinationInitialBalance + amount
            if (limit - sum < 0){
                return false
            }else{
@@ -75,26 +53,6 @@ function checkLimitBalance(account_id,amount) {
            }
         })
 }
-
-function insertTransaction(transactionobj) {
-    return model.sequelize.transaction((t) => {
-        return model.transaction.create(transactionobj, { transaction: t })
-            .then((result) => {
-                p1 = account.updateAccountBalance2("1233")
-                p2 = account.updateAccountBalance2("1222")
-                Promise.all([p1, p2]).then(values => {
-                    return values
-                }).catch(error => {
-                    throw new error.toString()
-                })
-            })
-    }).then((result) => {
-        return result
-    }).catch((error) => {
-        console.log(error.toString())
-    })
-}
-
 function insertTransaction(transactionObj) {
     let resultTran
     return model.sequelize.transaction((t) => {
@@ -116,7 +74,9 @@ function insertTransaction(transactionObj) {
                             })
                     }, { transaction: t })
                     .then((result) => {
-                        return resultTran
+                        console.log("--------------------------")
+                        console.log(resultTran)
+                        return result
                     })
                     .catch((error) => {
                         throw error
@@ -138,6 +98,5 @@ module.exports = {
     getTransactionInfo,
     checkAccountExist,
     insertTransaction,
-    checkEnoughBalance,
     checkLimitBalance
 }
--- app.js
+++ app.js
@@ -27,11 +27,11 @@ app.get("/insert", (req, res) => {
             tel: "0860755482",
             username: "Oh.tnp",
             password: "12345A",
-            balance: 200.0,
+            balance: 0.0,
             register_timestamp: '2017-07-25 09:29:00'
         },
         {
-            account_id: 9876543210,
+            account_id: 0000010001,
             name: "Thanaporn",
             surname: "Suwathanawongchai",
             citizen_id: "1111111111111",
@@ -57,7 +57,7 @@ app.get("/insert", (req, res) => {
     ]
    
      
-        controller.insertAccount(account).then((account) => {
+        controller.insertAccount(account[1]).then((account) => {
             res.send(account)
         })
 
@@ -66,12 +66,16 @@ app.get("/insert", (req, res) => {
 
 
 app.get("/accounts/:id", (req, res) => {
-    controller.getAccountInfo(req.params.id, ['account_id', 'name', 'surname','balance']).then((accounts) => {
+    controller.getAccountInfo(req.params.id, ['account_id', 'name', 'surname']).then((accounts) => {
         res.send(accounts)
     })
 
 })
-
+app.get("/balances/:id", (req, res) => {
+    controller.getAccountInfo(req.params.id, ['account_id', 'balance']).then((accounts) => {
+        res.send(accounts)
+    })
+})
 app.get("/transfer", (req, res) => {
     // const trans = {
     //     type: req.body.type,
@@ -84,71 +88,25 @@ app.get("/transfer", (req, res) => {
     //     src_remain_balance: req.body.src_remain_balance,
     //     des_remain_balance: req.body.des_remain_balance
     // }
-    src_acc_id = "1234567890" 
-    des_acc_id = "9876543210"
-    amount = 200
 
+    const trans = {
 
-    // checkaccount exist
-        controller.checkAccountExist(src_acc_id).then((isSrcExist)=>{
-            if(isSrcExist){
-                controller.checkAccountExist(des_acc_id).then((isDesExist)=>{
-                    if(isDesExist){
-                        // check Sender enough Balance
-                        controller.checkEnoughBalance(src_acc_id,amount).then((isEnoungh)=>{
-                            if(isEnoungh){
-                                // check Limit Reciever Balance exceed
-                                // TODO
-                                 const trans = {
+        type: "transfer",
+        src_account_id: 4097,
+        src_acc_id: 6302335476,
+        src_initial_balance: 4700,
+        des_account_id: 1234567890,
+        des_acc_id: 7582983660,
+        des_initial_balance: 4200,
+        amount: 500,
+        fee: 0,
+        src_remain_balance: 4500,
+        des_remain_balance: 4300
 
-                                    type: "transfer",
-                                    src_account_id: src_acc_id,
-                                    src_initial_balance: 200,
-                                    des_account_id: des_acc_id,
-                                    des_acc_id: des_acc_id,
-                                    des_initial_balance: 4700,
-                                    amount: amount,
-                                    fee: 0,
-                                    src_remain_balance: 0,
-                                    des_remain_balance: 4900
-
-                                }
-                                controller.insertTransaction(trans).then((response) => {
-                                    res.send(response)
-                                })
-                            }
-                        })
-                        
-                    }
-                })
-            }
-        })
-
-    
-    
-    
-
-    
-
-    // check 
-
-    // const trans = {
-
-    //     type: "transfer",
-    //     src_account_id: 4097,
-    //     src_initial_balance: 4700,
-    //     des_account_id: 1234567890,
-    //     des_acc_id: 7582983660,
-    //     des_initial_balance: 4200,
-    //     amount: 500,
-    //     fee: 0,
-    //     src_remain_balance: 4500,
-    //     des_remain_balance: 4300
-
-    // }
-    // controller.insertTransaction(trans).then((response) => {
-    //     res.send(response)
-    // })
+    }
+    controller.insertTransaction(trans).then((response) => {
+        res.send(response)
+    })
 
 })
 
