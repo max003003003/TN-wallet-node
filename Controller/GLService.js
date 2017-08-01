f76343bfa0a210f97577ccb7cf5faf1f23ceba32
Pawit Manussirivitaya <democryst@gmail.com>
2017-08-01 12:02:41 +0700

    modify GLservice

--- Controller/GLService.js
+++ Controller/GLService.js
@@ -1,8 +1,45 @@
 const model = require('../Model')
 const GLService={
-    createForTransactionTransferTo: ()=>{},
-    createForTransactionRecieveFrom: ()=>{},
-    checkBalanceforGL: ()=> {},
+    createForTransactionTransferTo: (amount, SourceId, transactionId)=>{
+      return {
+        Debit:{
+          Action: 'Saving',
+          Amount: amount,
+          Type: 'L'
+        },
+        Credit:{
+          Action: 'Cash',
+          Amount: amount,
+          Type: 'A'
+        },
+        SourceId: SourceId,
+        TransactionId: transactionId
+      }
+    },
+    createForTransactionRecieveFrom: (amount, DestinationId, transactionId)=>{
+      return {
+        Debit:{
+          Action: 'Cash',
+          Amount: amount,
+          Type: 'A'
+        },
+        Credit:{
+          Action: 'Saving',
+          Amount: amount,
+          Type: 'L'
+        },
+        DestinationId: DestinationId,
+        TransactionId: transactionId
+      }
+    },
+    checkBalanceforGL: (GLobjectTransactionTransferTo, GLobjectTransactionRecieveFrom)=> {
+      let DebitAmount = GLobjectTransactionTransferTo.Debit.Amount + GLobjectTransactionRecieveFrom.Debit.Amount;
+      let CreditAmount = GLobjectTransactionTransferTo.Credit.Amount + GLobjectTransactionRecieveFrom.Credit.Amount;
+      if (DebitAmount == CreditAmount) {
+        return true;
+      }
+      return false;
+    },
     insertGL: ()=>{}
 
 }
