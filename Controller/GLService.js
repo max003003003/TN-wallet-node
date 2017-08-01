4195f7369cf220deaa3da4925ebb32789f160384
vorabhol chauybhan <vorabhol.c@ku.th>
2017-08-01 11:30:50 +0700

    add GLService

---
+++ Controller/GLService.js
@@ -0,0 +1,10 @@
+const model = require('../Model')
+const GLService={
+    createForTransactionTransferTo: ()=>{},
+    createForTransactionRecieveFrom: ()=>{},
+    checkBalanceforGL: ()=> {},
+    insertGL: ()=>{}
+
+}
+
+module.exports = GLService
