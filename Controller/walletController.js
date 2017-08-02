233df3a9876cde226e2eea92ac5c5ea5705579ea
vorabhol chauybhan <vorabhol.c@ku.th>
2017-08-02 10:29:16 +0700

    change insertTransaction

--- Controller/walletController.js
+++ Controller/walletController.js
@@ -91,7 +91,7 @@ async function insertTransaction(transactionObj) {
         if (transactionResult[0]) return currentTransaction.dataValues.id
         //    throw new Error("transfer log error")    
     }
-    let transactionResult = await transactionService.updateTransactionsInstance(currentTransaction.dataValues.id, "TRANSFER MONEY FAIL")
+        let transactionResult = await transactionService.updateTransactionsInstance(currentTransaction.dataValues.id, "TRANSFER MONEY FAIL")
 
     
     // throw new Error("transfer failed") 
@@ -117,6 +117,7 @@ module.exports = {
     getTransactionInfo,
     checkAccountExist,
     checkDifferentAccount,
+    checkEnoughBalance,
     insertTransaction,
     checkEnoughBalance,
     checkLimitBalance,
