fc5241465bed7c7e64d8e4678eb7d19515c911a7
Jane <jane.kansiree@gmail.com>
2017-08-02 10:49:44 +0700

    add concurecy.test.js to github

---
+++ __test__/concurency.test.js
@@ -0,0 +1,68 @@
+require('mysql2/node_modules/iconv-lite').encodingExists('utf-8');
+//see https://github.com/sidorares/node-mysql2/issues/489
+const config = require('dotenv').config()
+
+var walletController = require('../Controller/walletController');
+var transactionService = require('../Controller/transactionService')
+const model = require('../Model')
+
+describe('testInsertTransaction', function () {
+    it('transfer at the same time', async () => {
+        const trans = {
+            type: "transfer",
+            src_account_id: 1888888883,
+            src_initial_balance: 1000,
+            des_account_id: 2888888884,
+            des_initial_balance: 1000,
+            amount: 100,
+            fee: 0.0,
+            src_remain_balance: 900,
+            des_remain_balance: 600
+        }
+        const trans1 = {
+            type: "transfer",
+            src_account_id: 3888888883,
+            src_initial_balance: 1000,
+            des_account_id: 2888888884,
+            des_initial_balance: 600,
+            amount: 100,
+            fee: 0.0,
+            src_remain_balance: 900,
+            des_remain_balance: 700
+        }
+        const trans2 = {
+            type: "transfer",
+            src_account_id: 1888888885,
+            src_initial_balance: 1000,
+            des_account_id: 2888888884,
+            des_initial_balance: 700,
+            amount: 100,
+            fee: 0.0,
+            src_remain_balance: 900,
+            des_remain_balance: 800
+        }
+        const trans3 = {
+            type: "transfer",
+            src_account_id: 5888888885,
+            src_initial_balance: 1000,
+            des_account_id: 2888888884,
+            des_initial_balance: 800,
+            amount: 100,
+            fee: 0.0,
+            src_remain_balance:900,
+            des_remain_balance: 900
+        }
+        var result = walletController.transferFund(trans).then((res)=>{
+                console.log("=====================================")
+            console.log(res)
+        })
+        var result1 = walletController.transferFund(trans1)
+        var result2 = walletController.transferFund(trans2)
+        var result3 = walletController.transferFund(trans3)
+        
+        // transactionService.deleteTransactionsInstance(result)
+        // transactionService.deleteTransactionsInstance(result1)
+        // transactionService.deleteTransactionsInstance(result2)
+        // transactionService.deleteTransactionsInstance(result3)
+    })
+})
